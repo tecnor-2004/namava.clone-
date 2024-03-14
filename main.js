10fb1fedbc92d1047b4bef4ba639e88cdeaa81a4
taha <tahairanpanah2004@gmail.com>
2024-03-14 11:36:05 +0330

    adding js for heaader

---
+++ main.js
@@ -0,0 +1,46 @@
+let navbar = $(".navbar")
+let lastPosition = 0;
+function scrollHandler(){
+    let Position = window.scrollY;
+    console.clear()
+    console.log("position :"+Position)
+    if(Position>=110){
+        navbar
+        .removeClass("position-absolute bg-transparent")
+        .addClass("position-fixed bg-primary")
+        .css({
+            "transition":"all 0s",
+            "top":"-120px"
+        })
+        if(Position > lastPosition && lastPosition <= Position){
+            lastPosition = Position
+            console.log("scrolling down")
+            navbar.css({
+                "box-shadow":"none",
+                "top":"-120px",
+                "transition":"all 1s"
+            })
+        }
+        else{
+            lastPosition = Position
+            console.log("scrolling up")
+            navbar.css({
+                "box-shadow":" 0 10px 7px rgba(0, 0, 0, .3)",
+                "top":"0px",
+                "transition":"all 1s"
+            })
+        }
+    }
+    else if(Position == 0){
+        navbar
+        .removeClass("position-fixed bg-primary")
+        .addClass("position-absolute bg-transparent")
+        .css({
+            "box-shadow":"none",
+            "top":"0px"
+        })
+    }
+}
+$(window).on("scroll", scrollHandler)
+  
+
